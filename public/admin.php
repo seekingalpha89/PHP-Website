19c4bdcd6af1c7db7e3062fbb0bb8d0f63ce0f0b
Alpha Sow <seekingalpha89@gmail.com>
2015-07-22 13:35:57 -0400

    linked stylesheet

--- public/admin.php
+++ public/admin.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>Untittled</title>
+<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
 <body>
 <div id="header">
 <h1>Widget Corp</h1>
