1c01c179adb383967b6d938a46d1f91b488f7f19
Alpha Sow <seekingalpha89@gmail.com>
2015-07-22 13:23:42 -0400

    started admin page skelleton

--- public/admin.php
+++ public/admin.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<?php
-
-$var1 =10;
-echo $var1;
-
-echo "<br />";
-
-$var1 =100;
-echo $var1;
-
-echo "<br />";
-
-$var2 ="Hello world!";
-echo $var2;
-
-?>
+<div id="header">
+<h1>Widget Corp</h1>
+</div>
+<div id="main">
+  <div id="navigation">
+    &nbsp;
+  </div>
+  <div id="page">
+    <h2>Admin Menu</h2>
+    <p>Welcome to the admin area.</p>
+    <ul>
+      <li><a href="manage_content.php">Manage Website Content</a></li>
+      <li><a href="manage_admins.php">Manage Admin Users</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+  </div>
+  <div id="footer">Copyright 20xx, Widget Corp</div>
+</div>
 
 </body>
 </html>
